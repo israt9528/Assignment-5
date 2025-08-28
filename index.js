885ea65fa4400fcfdcf1e1a0ea42af5900bee4f5
israt <israt9528@gmail.com>
2025-08-29 02:52:37 +0600

    added the call button feature

--- index.js
+++ index.js
@@ -8,8 +8,32 @@ function getValue(id) {
 const heart = document.getElementsByClassName("heart-icon");
 for (let icon of heart) {
   icon.addEventListener("click", function () {
-    count = getValue("count") + 1;
+    let count = getValue("count") + 1;
     const newCount = document.getElementById("count");
     newCount.innerText = count;
+  });
+}
+
+const call = document.getElementsByClassName("call-btn");
+for (let btn of call) {
+  btn.addEventListener("click", function (e) {
+    // check for sufficient coins
+    let coin = getValue("coin");
+    if (coin < 20) {
+      alert(`emergency-service.netlify.app says
+ You don't have sufficient coins! Need at least 20 coins to make a call.`);
+      return;
+    }
+    // make alert for click
+    let serviceName =
+      e.target.parentNode.parentNode.querySelector(".service-name").innerText;
+    let serviceNumber =
+      e.target.parentNode.parentNode.querySelector(".number").innerText;
+    alert(`emergency-service.netlify.app says
+ Calling ${serviceName} ${serviceNumber}...`);
+    // reduce coin
+    coin = coin - 20;
+    const newCoin = document.getElementById("coin");
+    newCoin.innerText = coin;
   });
 }
